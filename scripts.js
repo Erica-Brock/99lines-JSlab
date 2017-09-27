1343a4b1a816b52641a3719f08716341edab6db6
Erica-Brock <Eady.Brock@gmail.com>
2017-09-27 12:57:12 -0500

    created callFriend() function

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var friends;
+friends=["Kiara", "Kirsten", "Tyffani", "Jeff", "Brandon"];
+function callFriend(friends){
+    for(var i=[0]; i<friends.length; i++){
+        console.log(friends[i]+ ":");
+    }
+}
+callFriend(friends);
