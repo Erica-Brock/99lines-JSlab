493794d8fc4408263cc8648d7ee3fcd30249ba88
Erica-Brock <Eady.Brock@gmail.com>
2017-09-27 14:31:42 -0500

    final commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-var friends;
-friends=["Kiara", "Kirsten", "Tyffani", "Jeff", "Brandon"];
+var friends=["Kiara", "Kirsten", "Tyffani", "Jeff", "Brandon"];
+function sing(friend){
+    console.log (friend +":")
+    for(var i=99; i>1; i--){
+        console.log( i + " lines of code in the file,", i + " lines of code; " , friend ," strikes one out clears it all out", i-1 +" lines of code in the file.");
+    }
+    console.log("1 line of code in the file, 1 line of code;", friend +" strikes one out, clears it all out, no more lines of code in the file." );
+}
 function callFriend(friends){
-    for(var i=[0]; i<friends.length; i++){
-        console.log(friends[i]+ ":");
+    for(var i=0; i<friends.length ; i++){
+        sing(friends[i]);
     }
 }
+
 callFriend(friends);
