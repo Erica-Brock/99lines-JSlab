20772a03444c7ebc707dfc93aa0a204c96ffe71d
Erica-Brock <Eady.Brock@gmail.com>
2017-09-27 16:22:28 -0500

    second solution notated

--- scripts.js
+++ scripts.js
@@ -13,3 +13,17 @@ function callFriend(friends){
 }
 
 callFriend(friends);
+/* This way will work as well and is much easier:
+
+var friends=["Tyffani", "Kiara", "Kirsten", "Jeff", "Brandon"];
+for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i] + ":");
+    for (var e = 99; e>1; e--) {
+        console.log(e + " lines of code in the file", e + " lines of code", friends[i] + " takes one out, clears it all out,", e-1 + " lines of code in the file.");
+    }  this ends this statement.
+
+        if (e = 1) {
+            console.log("1 line of code in the file, 1 line of code;", i + " takes one out, clears it all out, no more lines of code in the file.");
+        } this ends this statement
+    } This ends the whole block
+    */
